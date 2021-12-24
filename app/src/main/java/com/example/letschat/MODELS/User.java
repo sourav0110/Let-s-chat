eac66d4a19bb0e4ef7a9e47903f4bde3e6701770
sourav0110 <souravbhattacharjee926@gmail.com>
2021-12-24 14:01:54 +0530

    sign up with email and password done

---
+++ app/src/main/java/com/example/letschat/MODELS/User.java
@@ -0,0 +1,71 @@
+package com.example.letschat.MODELS;
+
+public class User {
+    String profilepic,UserName,mail,password,userId,lastMessage;
+
+    public User(String profilepic, String userName, String mail, String password, String userId, String lastMessage) {
+        this.profilepic = profilepic;
+        UserName = userName;
+        this.mail = mail;
+        this.password = password;
+        this.userId = userId;
+        this.lastMessage = lastMessage;
+    }
+    public User(){}
+
+    //signUp constructor
+    public User(String userName, String mail, String password) {
+        this.UserName = userName;
+        this.mail = mail;
+        this.password = password;
+    }
+
+
+    public String getProfilepic() {
+        return profilepic;
+    }
+
+    public void setProfilepic(String profilepic) {
+        this.profilepic = profilepic;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+}
--- app/src/main/java/com/example/letschat/signUpActivity.java
+++ app/src/main/java/com/example/letschat/signUpActivity.java
@@ -1,15 +1,27 @@
 package com.example.letschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.example.letschat.MODELS.User;
 import com.example.letschat.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class signUpActivity extends AppCompatActivity {
     ActivitySignUpBinding binding;
     private FirebaseAuth auth;
+    private FirebaseDatabase database;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,36 @@ public class signUpActivity extends AppCompatActivity {
         binding=ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+        progressDialog=new ProgressDialog(signUpActivity.this);
+        progressDialog.setTitle("Creating account");
+        progressDialog.setMessage("We are creating your account");
+        binding.SignupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.UsernameSignUpET.getText().toString()!=""){
+                    Toast.makeText(signUpActivity.this,"Username cannot be empty",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    progressDialog.show();
+                    auth.createUserWithEmailAndPassword(binding.UserSignUpEmailET.getText().toString(), binding.userPasswordSignUpET.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    progressDialog.dismiss();
+                                    if (task.isSuccessful()) {
+                                        User user = new User(binding.UsernameSignUpET.getText().toString(), binding.UserSignUpEmailET.getText().toString(), binding.userPasswordSignUpET.getText().toString());
+                                        String id = task.getResult().getUser().getUid();
+                                        database.getReference().child("Users").child(id).setValue(user);
+                                        Toast.makeText(signUpActivity.this, "User created successfully", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(signUpActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
 
 
     }
